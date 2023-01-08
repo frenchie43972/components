30a52a221d3af84c9a96bb5b35a7db1efccc8551
Kris French <frenchie43972@gmail.com>
2023-01-08 16:33:26 +0900

    fifnished accordion

--- components/src/components/Accordion.js
+++ components/src/components/Accordion.js
@@ -1,16 +1,48 @@
+import { useState } from "react";
+import { ImCircleDown, ImCircleLeft } from "react-icons/im";
+
 function Accordion({items}) {
-    const renderedItems = items.map((item) => {
+    // The useState(-1) sets a false valuse so IsExpanded defaults to false
+    // so the accordion is collapsed
+    const [expandedIndex, setExpandedIndex] = useState(-1);
+
+    // This will default close all the items and allow you to open/close as 
+    // needed
+    const handleClick = (nextIndex) => {
+        if (expandedIndex === nextIndex) {
+            setExpandedIndex(-1);
+        } else {
+            setExpandedIndex(nextIndex);
+        }
+        
+    };
+
+    const renderedItems = items.map((item, index) => {
+        // This variable produces a boolean 
+        const isExpanded = index === expandedIndex;   
+        
+        const icon = <span className="text-xl">
+            {/* Ternay expression to dispay an icon depanding on the state of   isExpanded (true or false) */}
+            {isExpanded ? <ImCircleDown /> : <ImCircleLeft />}
+        </span>
+
         return (
             <div key={item.id}>
-                <div>{item.label}</div>
-                <div>{item.content}</div>
+                {/* This has an inline onClick event handler that will set the index to the array index item taht was clicked */}
+                <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>
+                    {item.label}
+                    {icon}
+                </div>
+                {/* This will look and if isExpanded is truthy, then it will
+                produce the last truthy value ( <div>{item.content}</div> ). If it isExpanded is falsy, it will show the first falsy value (isExpanded) */}
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
             
         );
     });
 
     return (
-        <div>
+        <div className="border-x border-t rounded">
             {renderedItems}
         </div>
     );

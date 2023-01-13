c73dd8c78be7c25e8a987b06075694391361198b
Kris French <frenchie43972@gmail.com>
2023-01-13 17:51:57 +0900

    working the Table

--- components/src/components/Table.js
+++ components/src/components/Table.js
@@ -1,10 +1,47 @@
 
 
-function Table() {
+function Table({data, config, keyFn}) {
+    // Maps over and retrieves all the information stored in config from
+    // TablePage. Then created a new header with that information so 
+    // when it is called in the <th> element it will show 
+    const renderedHeaders = config.map((column) => {
+        return (
+            <th key={column.label}>
+                {column.label}
+            </th>
+        );
+    });
+
+    // Maps over and retrieves all the information stored in data from
+    // TablePage. Then created a new row with that information so 
+    // when it is called in the tboy element it will show everything
+    const renderedRows = data.map((rowData) => {
+        const renderedCells = config.map((column) => {
+            return (
+                <td className="p-2" key={column.label}>
+                    {column.render(rowData)}
+                </td>
+            );
+        });
+
+        return (
+            <tr className="border-b" key={keyFn(rowData)}>
+                {renderedCells}
+            </tr>
+        );
+    });
+    
     return (
-        <div>
-            Table
-        </div>
+        <table className="table-auto border-spacing-2">
+            <thead>
+                <tr className="border-b-2">
+                    {renderedHeaders}
+                </tr>
+            </thead>
+            <tbody>
+                {renderedRows}
+            </tbody>
+        </table>
     );
 }
 
